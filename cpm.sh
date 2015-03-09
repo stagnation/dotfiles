b915921d425865bc6632150759202b1d13f020b8
Nils <nils@wireklint.se>
2015-03-09 16:57:02 +0100

    scripts moved to /bin

--- cpm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-setxkbmap -layout us;
-quake3 +set fs_game cpma;
-setxkbmap -layout se -variant mac -option ctrl:nocaps;
--- wsw.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-setxkbmap -layout en_US
-~/game/warsow_15/warsow
-setxkbmap -layout se -variant mac -option ctrl:nocaps
