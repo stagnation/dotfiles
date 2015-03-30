5de9907c6d75a59b47e49ce67e28d2f7891404c6
Nils <nils@wireklint.se>
2015-03-30 21:29:17 +0200

    better url to clipboard

--- bin/prtscr.sh
+++ bin/prtscr.sh
@@ -4,5 +4,6 @@ pushd ~/screenshots
 scrot
 scrot newest.png
 scp newest.png spill@aelv.se:~/public_html/screen.png
-echo "aelv.se/spill/screen.png" | xclip -i -selection clipboard
+echo "www.aelv.se/spill/screen.png" | xclip -i -selection clipboard
+echo "aelv.se/spill/screen.png saved in clipboard"
 popd
