511bc78a8876d532e56db7efdfe20f0001f484e6
Nils <nils@wireklint.se>
2015-04-17 14:06:10 +0200

    fixed public url

--- bin/prtscr.sh
+++ bin/prtscr.sh
@@ -4,6 +4,6 @@ pushd ~/screenshots
 scrot
 scrot newest.png
 scp newest.png spill@aelv.se:~/public_html/screen.png
-echo "www.aelv.se/spill/screen.png" | xclip -i -selection clipboard
-echo "aelv.se/spill/screen.png saved in clipboard"
+echo "http://www.aelv.se/spill/screen.png" | xclip -i -selection clipboard
+echo "http://www.aelv.se/spill/screen.png saved in clipboard"
 popd
