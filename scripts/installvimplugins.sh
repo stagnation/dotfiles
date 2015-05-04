a74535c3cc2e58015d0d2ac91cab194fbd6c809b
Nils <nils@wireklint.se>
2015-05-04 12:30:09 -0400

    script to install plugins for vim

---
+++ scripts/installvimplugins.sh
@@ -0,0 +1,27 @@
+git clone https://github.com/michaeljsmith/vim-indent-object ~/.vim/bundle/vim-indent-object
+git clone https://github.com/scrooloose/syntastic/           ~/.vim/bundle/syntastic
+git clone https://github.com/majutsushi/tagbar               ~/.vim/bundle/tagbar
+git clone https://github.com/tpope/vim-fugitive              ~/.vim/bundle/vim-fugitive
+git clone https://github.com/bronson/vim-trailing-whitespace ~/.vim/bundle/vim-trailing-whitespace
+git clone https://github.com/whatyouhide/vim-lengthmatters   ~/.vim/bundle/vim-lengthmatters
+git clone https://github.com/vim-scripts/camelcasemotion     ~/.vim/bundle/camelcasemotion
+git clone https://github.com/Osse/double-tap                 ~/.vim/bundle/double-tap
+git clone https://github.com/wellle/targets.vim              ~/.vim/bundle/targets.vim
+git clone https://github.com/mbbill/undotree                 ~/.vim/bundle/undotree
+git clone https://github.com/airblade/vim-gitgutter          ~/.vim/bundle/vim-gitgutter
+git clone https://github.com/nathanaelkane/vim-indent-guides ~/.vim/bundle/vim-indent-guides
+git clone https://github.com/mhinz/vim-startify              ~/.vim/bundle/vim-startify
+git clone https://github.com/pelodelfuego/vim-swoop          ~/.vim/bundle/vim-swoop
+git clone https://github.com/christoomey/vim-tmux-navigator  ~/.vim/bundle/vim-tmux-navigator
+git clone https://github.com/kshenoy/vim-signature           ~/.vim/bundle/vim-signature
+git clone https://github.com/kien/ctrlp.vim                  ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/godlygeek/tabular               ~/.vim/bundle/tabular
+git clone https://github.com/tpope/vim-commentary            ~/.vim/bundle/vim-commentary
+
+# macro to grab end of github link and write corresponding plugin path
+# 0f/;;;lvEyA ~/.vim/bundle/p0
+
+# align on ~
+# :Tabularize /\~<cr>
+# fix '~' generated
+# :%s/\~\ /\~/<cr>
