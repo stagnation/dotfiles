0905f9d88235be9597790648d40dc47bcae5a6ea
User Spill <spill@born.aelv.se>
2015-05-06 19:56:02 +0200

    updated install script to include pathogen, and create backup dir

--- scripts/installvimplugins.sh
+++ scripts/installvimplugins.sh
@@ -1,3 +1,7 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/backup
+
 git clone https://github.com/michaeljsmith/vim-indent-object ~/.vim/bundle/vim-indent-object
 git clone https://github.com/scrooloose/syntastic/           ~/.vim/bundle/syntastic
 git clone https://github.com/majutsushi/tagbar               ~/.vim/bundle/tagbar
