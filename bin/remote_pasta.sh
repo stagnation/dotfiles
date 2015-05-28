5f37441d9e2610aafa62cb1f406489b913283bd0
Nils <nils@wireklint.se>
2015-05-28 18:53:16 +0200

    upload script, allow uploading clipboard ocntent directly

--- bin/remote_pasta.sh
+++ bin/remote_pasta.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 #take filename argument.
 
+FILE="/home/spill/scratch/remotepasta"
 if [ $# -ge 1 ] ; then
     if [ -f ${1} ] ; then
-
-        FILE="/home/spill/scratch/remotepasta"
         cp $1 $FILE
-        chmod -rwx $FILE
-        chmod 644 $FILE
-        scp $FILE spill@aelv.se:~/public_html/pasta.txt
-        if [ $# -ge 2 ] ; then
-            echo http://aelv.se/spill/pasta.txt | xclip -i -selection clipboard
-            echo http://aelv.se/spill/pasta.txt | xclip -i -selection primary
-            echo "http://aelv.se/spill/pasta.txt copied to clipboard"
-        fi
+    else
+        echo "illegal file specified"
     fi
+else
+    #if no file upload contents of clipboard
+    xclip -o -selection clipboard > $FILE
+fi
+chmod -rwx $FILE
+chmod 644 $FILE
+scp $FILE spill@aelv.se:~/public_html/pasta.txt
+if [ $# -ge 2 ] ; then
+    echo http://aelv.se/spill/pasta.txt | xclip -i -selection clipboard
+    echo http://aelv.se/spill/pasta.txt | xclip -i -selection primary
+    echo "http://aelv.se/spill/pasta.txt copied to clipboard"
 fi
