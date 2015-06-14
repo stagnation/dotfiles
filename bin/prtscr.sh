4219ff86af4a935b12a7d9b0f6a6449eebee14f3
Nils <nils@wireklint.se>
2015-06-14 09:48:07 +0200

    bin: updated printscreen to ecxho url to clipbaord before scp

--- bin/prtscr.sh
+++ bin/prtscr.sh
@@ -3,8 +3,8 @@
 pushd ~/screenshots
 scrot
 scrot newest.png
-scp newest.png spill@aelv.se:~/public_html/screen.png
 echo "http://www.aelv.se/spill/screen.png" | xclip -i -selection clipboard
 echo "http://www.aelv.se/spill/screen.png" | xclip -i -selection primary
 echo "http://www.aelv.se/spill/screen.png saved in clipboard"
+scp newest.png spill@aelv.se:~/public_html/screen.png
 popd
