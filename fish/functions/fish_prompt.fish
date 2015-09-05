7f160729f9814bf63c74ab3571c1a925bb0e609f
Nils <nils@wireklint.se>
2015-09-05 13:36:49 +0200

    fish: removed vi binds/prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,22 +1,6 @@
 # name: GitStatus
 # Find latest version from: https://github.com/godfat/fish_prompt-gitstatus
-set -g fish_key_bindings fish_vi_key_bindings
 
-function fish_vi_prompt_cm --description "Displays the current mode"
-  #echo -n " "
-  switch $fish_bind_mode
-    case default
-      #set_color --bold --background red white
-      echo "[]"
-    case insert
-      #set_color --bold --background green white
-      echo "~>"
-    case visual
-      #set_color --bold --background magenta white
-      echo "[V]"
-  end
-  set_color normal
-end
 
 function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
@@ -53,5 +37,5 @@ function fish_prompt
     set git_status " $git_status"
   end
 
-  echo -n (_remote_hostname) $cwd$cyan$git_status$normal (fish_vi_prompt_cm)
+  echo -n (_remote_hostname) $cwd$cyan$git_status$normal :   
 end
