002c5bbb4d857b6076f4b0435a9708a17126de00
Nils Wireklint <nils@wireklint.se>
2015-11-30 18:08:11 +0100

    bin: proper error handling of mpdvol

--- bin/mpdvol.sh
+++ bin/mpdvol.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-output=$(mpc -h 192.168.1.8 volume 2> /dev/null || echo "")
-echo ${output:7:12}
+ping 192.168.1.8 -c 1 -q -w 0.1 &>/dev/null
+if [ $? -ne 1 ] ; then
+    output=$(mpc -h 192.168.1.8 volume 2> /dev/null || echo "")
+    output=${output:7:12}
+    echo $output
+else
+    echo "mpd down"
+fi
+exit 0
