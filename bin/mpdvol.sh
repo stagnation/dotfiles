3159e149b4f3e8b5a1ec9af9f114e19b95ffe8c5
Nils Wireklint <nils@wireklint.se>
2015-12-10 18:43:14 +0100

    bin: mpdvol for i3pystatus improvement

--- bin/mpdvol.sh
+++ bin/mpdvol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ping 192.168.1.8 -c 1 -q -w 0.1 &>/dev/null
+ping 192.168.1.8 -c 1 -q -w 0.01 &>/dev/null
 if [ $? -ne 1 ] ; then
     output=$(mpc -h 192.168.1.8 volume 2> /dev/null || echo "")
     output=${output:7:12}
