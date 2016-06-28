ddb6c999bee941e65208bcf534de87bdf7baa319
Nils Wireklint <nils@wireklint.se>
2016-06-28 19:09:29 +0200

    serv: updated fish and tmux cfg

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,3 @@
-function tmux_attach
-	tmux has-session -t remote
-	and tmux attach-session -t remote
-end
-
-function tmux_new_session
-	bash ~/bin/createtmuxsession.sh
-end
-
-if status --is-login
-	tmux_attach
-	or tmux_new_session
-	or echo "tmux failed to start; plain shelll"
-end
-
 function naelv
     urxvt -e sh -c "mosh aelv -- screen -dr" &
 end
@@ -29,6 +14,9 @@ function pstime
     ps -eo "%p %y %t %c"
 end
 
+if status --is-login
+	sh ~/dotfiles/bin/createtmuxsession
+end
 
 set PATH /home/spill/.local/bin $PATH
 set PATH /home/spill/bin $PATH
