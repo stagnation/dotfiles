388854d7235ab75bf6557b8fff73af237e913bba
Nils Wireklint <nils@wireklint.se>
2016-07-06 12:16:07 +0200

    i3/bin: better float terminal with i3 rule

--- bin/ircfloat.sh
+++ bin/ircfloat.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-delay=0.35
-urxvt -depth 32 -bg rgba:0a00/1000/1500/dddd -e sh -c "mosh aelv" &
-sleep $delay
-i3-msg floating enable >/dev/null
-i3-msg move right 475 >/dev/null
-i3-msg resize shrink left 200 >/dev/null
-i3-msg move to scratchpad
-i3-msg scratchpad show
+command="mosh aelv"
+if [ $# -ge 1 ] ; then
+    command=$1
+fi
+echo $command
+
+urxvt -depth 32 -bg rgba:0a00/1000/1500/dddd -name "float_term" & 2> /dev/null
+
+# i3-msg floating enable >/dev/null
+# i3-msg move right 475 >/dev/null
+# i3-msg resize shrink left 200 >/dev/null
+# i3-msg move to scratchpad
+# i3-msg scratchpad show
