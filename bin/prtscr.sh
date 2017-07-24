0eb3331a910b83d695d374ba69c739db087f95cf
Nils Wireklint <nils@wireklint.se>
2017-07-24 15:25:30 +0200

    bin: update prtscr screen shot

--- bin/prtscr.sh
+++ bin/prtscr.sh
@@ -4,9 +4,14 @@ screenshotdir=~/screenshot
 time=$(date +%s)
 mkdir -p $screenshotdir
 
-scrot "$screenshotdir"/"$time".png
+localscreenpath="$screenshotdir"/"$time".png
+scrot $localscreenpath
+
+ssh spill@aelv "mkdir -p ~/public_html/screens"
+
+scp $localscreenpath spill@aelv:~/public_html/screen.png
+scp $localscreenpath spill@aelv:~/public_html/screens/"$time".png
+
 echo "http://www.aelv.se/spill/screen.png" | xclip -i -selection clipboard
 echo "http://www.aelv.se/spill/screen.png" | xclip -i -selection primary
 echo "http://www.aelv.se/spill/screen.png saved in clipboard"
-scp "$screenshotdir"/"$time".png spill@aelv:~/public_html/screen.png
-scp "$screenshotdir"/"$time".png spill@aelv:~/public_html/"$time".png
