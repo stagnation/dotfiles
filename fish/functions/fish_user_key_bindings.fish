9386ccd7c8081ce8dc8fb47575d60b15e4e80819
Nils Wireklint <nils@wireklint.se>
2018-01-21 11:23:15 +0100

    fish: <m-h> support help for subcommands

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -6,19 +6,22 @@ end
 
 function __write_help
     set -l command $argv[1]
-    if test -z (which $command)
+    # show help specific to a subcommand if one is given, otherwise this variable is expanded to an empty string.
+    set -l subcommand $argv[2]
+
+    if test -z (which "$command")
         return 1
     end
 
     set -l help_output_storage (mktemp)
-    eval $command --help > $help_output_storage
+    eval "$command" "$subcommand" --help > "$help_output_storage"
     set -l ok $status
 
-    if test $ok -eq 0
+    if test "$ok" -eq 0
         less "$help_output_storage"
     end
 
-    return (echo $ok)
+    return (echo "$ok")
 end
 
 function __fish_man_page2
@@ -35,12 +38,15 @@ function __fish_man_page2
     # Try "man first-second" and fall back to "man first" if that doesn't work out.
     set -l maincmd (basename $args[1])
     if set -q args[2]
-        man "$maincmd-$args[2]" ^/dev/null
+        set -l subcommand "$args[2]"
+        man "$maincmd-$subcommand" ^/dev/null
+        or __write_help "$maincmd" "$subcommand"
         or man "$maincmd" ^/dev/null
+        or __write_help "$maincmd"
         or printf \a
     else
         man "$maincmd" ^/dev/null
-        or __write_help "$maincmd"
+        __write_help "$maincmd"
         or printf \a
     end
 
