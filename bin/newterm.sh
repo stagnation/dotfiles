daaa96d5c38177857eb724e047f79af93efc3fea
Nils Wireklint <nils@wireklint.se>
2018-04-11 18:46:38 +0200

    tmux: pad session name to 8 characters if shorter

--- bin/newterm.sh
+++ bin/newterm.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-create_new_session="new-session -s _$(random_word.sh)"
-urxvt -cd "`xcwd`" -e tmux -u new-session -s _`random_word.sh` || urxvt
+word="$(random_word.sh)"
+name_length=$(echo $word | wc -c)
+padding_required=$((9 - $name_length))
+padding=$(head -c $padding_required < /dev/zero | tr '\0' ' ')
+session_name="$padding$word"
+
+urxvt -cd "`xcwd`" -e tmux -u new-session -s "_$session_name" || urxvt
