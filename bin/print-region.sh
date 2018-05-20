c29d33665f590b85383d22ed76e1ea9c4e451b8b
Nils Wireklint <nils@wireklint.se>
2018-05-20 14:25:59 +0200

    i3/bin: bind print-region in i3

--- bin/print-region.sh
+++ bin/print-region.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ~/screenshots
+cd ~/screenshots || exit 1
 
 unixtime=$(date +%s)
 filename=${unixtime}.png
@@ -8,7 +8,13 @@ filename=${unixtime}.png
 # NB(nils): this seems to be scrot default, but for clarify, and to have the
 #           filename in a variable.
 
-scrot -s "$filename"
+sleep 0.2
+# NB(nils): this is a work around around a scrot/giblib error:
+#           giblib error: couldn't grab pointer:Resource temporarily unavailable
+#           https://bbs.archlinux.org/viewtopic.php?id=86507
+#           https://code.google.com/archive/p/xmonad/issues/476
+
+scrot -s "$filename" || exit 1
 
 echo "http://www.aelv.se/spill/${filename}" | xclip -i -selection clipboard
 echo "http://www.aelv.se/spill/${filename}" | xclip -i -selection primary
