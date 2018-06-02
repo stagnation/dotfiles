07ff06396752f991e36e7f705fa304ca3e502d12
Nils Wireklint <nils@wireklint.se>
2018-06-02 16:42:35 +0200

    i3: use bindsym --release to avoid sleep in scrot

--- bin/print-region.sh
+++ bin/print-region.sh
@@ -8,12 +8,6 @@ filename=${unixtime}.png
 # NB(nils): this seems to be scrot default, but for clarify, and to have the
 #           filename in a variable.
 
-sleep 0.2
-# NB(nils): this is a work around around a scrot/giblib error:
-#           giblib error: couldn't grab pointer:Resource temporarily unavailable
-#           https://bbs.archlinux.org/viewtopic.php?id=86507
-#           https://code.google.com/archive/p/xmonad/issues/476
-
 scrot -s "$filename" || exit 1
 
 echo "http://www.aelv.se/spill/${filename}" | xclip -i -selection clipboard
